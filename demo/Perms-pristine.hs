9530ddbd8ca68d3f0ed54b8fd09401e36a89f364
Brent Yorgey <byorgey@gmail.com>
2015-02-09 00:51:15 -0600

    Perms demo

---
+++ demo/Perms-pristine.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Data.List
+import Data.List.Split
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+type Dia = Diagram B R2
+
+colors = [red, green, blue, orange]
